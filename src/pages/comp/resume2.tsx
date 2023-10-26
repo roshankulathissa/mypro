8045610ef144a696f6845b37e2bac3ab12f846a3
Roshan Madumadawa <roshankulathissa@gmail.com>
2023-10-26 21:40:10 +0530

    Update the download link and change the button text and size (generated by useblackbox.ai )

---
+++ src/pages/comp/resume2.tsx
@@ -0,0 +1,23 @@
+import { motion } from "framer-motion";
+
+function Resume() {
+    return (
+        <div className=" flex justify-center">
+        <motion.div
+            whileHover={{ scale: 1.8 }} whileTap={{ scale: 0.9 }}
+>
+    <a
+        href="https://drive.google.com/file/d/1Uoi364PdsMUj1AyPaZjHdnb4SlFQc7X4/view?usp=share_link"
+    download={"resume"}
+    target="blank"
+    className=" flex bg-yellow-400 text-blue-700 w-16 py-2
+    border-none rounded-md justify-center"
+    >
+    resume
+    </a>
+    </motion.div>
+    </div>
+);
+}
+
+export default Resume;
